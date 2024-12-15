d6a0acc47f3ce553d2f5c2596f68573606dc9040
Namir50 <namirsayyed50@gmail.com>
2024-12-15 14:35:02 +0530

    New

--- Course/Multiprocessing_and_Multithreading/1.py
+++ Course/Multiprocessing_and_Multithreading/1.py
@@ -3,3 +3,19 @@ Multithreading
 When to use Multithreading: 
   I/O bound tasks: tasks that spend more time wwwaiting for I/O operations(e.g.: file operation, network requests)
   Concurrent operations: when you want to improve the throughput of your application by performing multiple operations concurrently"""
+  
+import threading
+import time
+
+def print_numbers():
+    for i in range(5):
+        print(f"Number:{i}")
+
+def print_letters():
+    for letter in "abcde":
+        print(f"Letter:{letter}")
+
+t = time.now()
+print_numbers()
+print_letters()
+print(f"Time take: {time.now() - t}")
