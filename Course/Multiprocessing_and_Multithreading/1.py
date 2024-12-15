8e200d4c45111452411bfd768de9c5d699694325
Namir50 <namirsayyed50@gmail.com>
2024-12-15 14:29:17 +0530

    New

---
+++ Course/Multiprocessing_and_Multithreading/1.py
@@ -0,0 +1,5 @@
+"""
+Multithreading
+When to use Multithreading: 
+  I/O bound tasks: tasks that spend more time wwwaiting for I/O operations(e.g.: file operation, network requests)
+  Concurrent operations: when you want to improve the throughput of your application by performing multiple operations concurrently"""
