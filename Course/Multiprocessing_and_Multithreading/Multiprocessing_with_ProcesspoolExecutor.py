416e0aec10b976ebe65ecbbb6c66f30238fd86ba
Namir50 <namirsayyed50@gmail.com>
2024-12-18 17:36:14 +0530

    New

--- Course/Multiprocessing_and_Multithreading/Multiprocessing_with_ProcesspoolExecutor.py
+++ Course/Multiprocessing_and_Multithreading/Multiprocessing_with_ProcesspoolExecutor.py
@@ -2,7 +2,7 @@ from concurrent.futures import ProcessPoolExecutor
 import time
 
 def numerical_values(numbers):
-    time.sleep(1)  #will execute faster if we dont use this function
+    time.sleep(1)  #will executefaster if we dont use this function
     return f"Numbers: {numbers}"
 
 numbers = [132,453,543,123,543,566]
