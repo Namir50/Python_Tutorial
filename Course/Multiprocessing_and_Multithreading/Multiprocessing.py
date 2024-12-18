f327a4f0331faddf593612bedf249b34edba0474
Namir50 <namirsayyed50@gmail.com>
2024-12-18 17:17:09 +0530

    New

--- Course/Multiprocessing_and_Multithreading/Multiprocessing.py
+++ Course/Multiprocessing_and_Multithreading/Multiprocessing.py
@@ -1,3 +1,31 @@
 #Processes that run in parallel
 #Used for tasks that are heavy on CPU(e.g.: mathematical computations, data processing)
 #Uses multiple cores of the CPU to speed up the process            
+
+import multiprocessing
+import time
+
+def square():
+    for i in range(5):
+        time.sleep(1)
+        print(f"Square: {i**2}")
+        
+def cube():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"Cube: {i**3}")
+        
+if __name__ == "__main__":  #just creating an entry point
+    p1 = multiprocessing.Process(target = square)
+    p2 = multiprocessing.Process(target = cube)
+    
+    t = time.time()
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    print(f"Time taken is : {time.time()-t}")
+    
