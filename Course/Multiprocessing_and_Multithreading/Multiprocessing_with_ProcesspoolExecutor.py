c697b68681e342d4d8e6d7646ac70e7ceefbbd3b
Namir50 <namirsayyed50@gmail.com>
2024-12-18 17:33:47 +0530

    ProcessPoolExecutor

---
+++ Course/Multiprocessing_and_Multithreading/Multiprocessing_with_ProcesspoolExecutor.py
@@ -0,0 +1,15 @@
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def numerical_values(numbers):
+    time.sleep(1)
+    return f"Numbers: {numbers}"
+
+numbers = [132,453,543,123,543,566]
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor(max_workers = 3) as executor:
+        results = executor.map(numerical_values,numbers)
+        
+    for result in results:
+        print(result)
