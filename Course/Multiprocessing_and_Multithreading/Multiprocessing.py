4133c208d2567d04c875596e2fdd417059a953f7
Namir50 <namirsayyed50@gmail.com>
2024-12-18 17:08:54 +0530

    New

---
+++ Course/Multiprocessing_and_Multithreading/Multiprocessing.py
@@ -0,0 +1,3 @@
+#Processes that run in parallel
+#Used for tasks that are heavy on CPU(e.g.: mathematical computations, data processing)
+#Uses multiple cores of the CPU to speed up the process            
